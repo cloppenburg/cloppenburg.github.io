8ce615e6619d6486ff7f5e42fe104afdd730f42b
André Schreck <github@andreschreck.de>
2023-12-19 16:53:11 +0100

    refactor

--- assets/js/galery.js
+++ assets/js/galery.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const allImages = Array.prototype.slice.call(document.querySelectorAll('#galery #images img'), 0);
-    // console.debug('children:', allImages);
     
     const imageContainer = document.querySelector('#galery #current-image')
 
@@ -8,24 +7,29 @@ document.addEventListener('DOMContentLoaded', () => {
     imageContainer.appendChild(allImages[0])
 
     document.querySelector('#galery #left').addEventListener('click', () => {
-        imageContainer.removeChild(allImages[selectedImageIndex])
-        // console.debug("oldIndex:", selectedImageIndex)
-        selectedImageIndex--;
-        if (selectedImageIndex < 0) {
-            selectedImageIndex = allImages.length - 1
-        }
-        // console.debug("newIndex:", selectedImageIndex)
-        imageContainer.appendChild(allImages[selectedImageIndex])
+        selectedImageIndex = setImage(imageContainer, selectedImageIndex, allImages, (imageIndex) => {
+            imageIndex--;
+            if (imageIndex < 0) {
+                imageIndex = allImages.length - 1
+            }
+            return imageIndex
+        })
     });
 
     document.querySelector('#galery #right').addEventListener('click', () => {
-        imageContainer.removeChild(allImages[selectedImageIndex])
-        // console.debug("oldIndex:", selectedImageIndex)
-        selectedImageIndex++;
-        if (selectedImageIndex > allImages.length - 1) {
-            selectedImageIndex = 0
-        }
-        // console.debug("newIndex:", selectedImageIndex)
-        imageContainer.appendChild(allImages[selectedImageIndex])
+        selectedImageIndex = setImage(imageContainer, selectedImageIndex, allImages, (imageIndex) => {
+            imageIndex++;
+            if (imageIndex > allImages.length - 1) {
+                imageIndex = 0
+            }
+            return imageIndex
+        })
     });
 });
+
+function setImage(imageContainer, oldImageIndex, allImages, indexCalculator) {
+    imageContainer.removeChild(allImages[oldImageIndex]);
+    const newImageIndex = indexCalculator(oldImageIndex);
+    imageContainer.appendChild(allImages[newImageIndex])
+    return newImageIndex;
+}
