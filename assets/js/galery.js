ba1ce76fa94914656ba1198dd081a84ec5e2b089
André Schreck <github@andreschreck.de>
2024-01-07 22:45:42 +0100

    galery supports opening of images

--- assets/js/galery.js
+++ assets/js/galery.js
@@ -7,6 +7,35 @@ document.addEventListener('DOMContentLoaded', () => {
         selectedImageIndex = 0;
         imageContainer.appendChild(allImages[0])
 
+        document.querySelector('#galery #current-image').addEventListener('click', () => {
+            const wall = document.createElement('div');
+            wall.id = 'wall'
+            wall.style.width = `${document.body.clientWidth}px`;
+            wall.style.height = `${document.body.clientHeight}px`;
+
+            const bigImageContainer = document.createElement('div');
+            bigImageContainer.id = 'big-image';
+            bigImageContainer.style.width = `${document.body.clientWidth-40}px`;
+            //bigImageContainer.style.height = `${document.body.clientHeight-20}px`;
+
+            bigImageContainer.appendChild(allImages[selectedImageIndex])
+            
+            document.body.appendChild(wall);
+            document.body.appendChild(bigImageContainer);
+
+            const closeListener = () => {
+                wall.removeEventListener('click', closeListener);
+                document.querySelector('#big-image img').removeEventListener('click', closeListener);
+
+                document.body.removeChild(bigImageContainer);
+                document.body.removeChild(wall);
+                imageContainer.appendChild(allImages[selectedImageIndex])
+            }
+
+            wall.addEventListener('click', closeListener);
+            document.querySelector('#big-image img').addEventListener('click', closeListener);
+        });
+
         document.querySelector('#galery #left').addEventListener('click', () => {
             selectedImageIndex = setImage(imageContainer, selectedImageIndex, allImages, (imageIndex) => {
                 imageIndex--;
