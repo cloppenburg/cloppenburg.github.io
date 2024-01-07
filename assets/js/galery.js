4ace14902ac73031f056a28d1607d31e438e2118
André Schreck <github@andreschreck.de>
2024-01-07 21:46:54 +0100

    fix galery

--- assets/js/galery.js
+++ assets/js/galery.js
@@ -1,30 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const allImages = Array.prototype.slice.call(document.querySelectorAll('#galery #images img'), 0);
-    
-    const imageContainer = document.querySelector('#galery #current-image')
+    if (document.querySelector('#galery')) {
+        const allImages = Array.prototype.slice.call(document.querySelectorAll('#galery #images img'), 0);
+        
+        const imageContainer = document.querySelector('#galery #current-image')
 
-    selectedImageIndex = 0;
-    imageContainer.appendChild(allImages[0])
+        selectedImageIndex = 0;
+        imageContainer.appendChild(allImages[0])
 
-    document.querySelector('#galery #left').addEventListener('click', () => {
-        selectedImageIndex = setImage(imageContainer, selectedImageIndex, allImages, (imageIndex) => {
-            imageIndex--;
-            if (imageIndex < 0) {
-                imageIndex = allImages.length - 1
-            }
-            return imageIndex
-        })
-    });
+        document.querySelector('#galery #left').addEventListener('click', () => {
+            selectedImageIndex = setImage(imageContainer, selectedImageIndex, allImages, (imageIndex) => {
+                imageIndex--;
+                if (imageIndex < 0) {
+                    imageIndex = allImages.length - 1
+                }
+                return imageIndex
+            })
+        });
 
-    document.querySelector('#galery #right').addEventListener('click', () => {
-        selectedImageIndex = setImage(imageContainer, selectedImageIndex, allImages, (imageIndex) => {
-            imageIndex++;
-            if (imageIndex > allImages.length - 1) {
-                imageIndex = 0
-            }
-            return imageIndex
-        })
-    });
+        document.querySelector('#galery #right').addEventListener('click', () => {
+            selectedImageIndex = setImage(imageContainer, selectedImageIndex, allImages, (imageIndex) => {
+                imageIndex++;
+                if (imageIndex > allImages.length - 1) {
+                    imageIndex = 0
+                }
+                return imageIndex
+            })
+        });
+    }
 });
 
 function setImage(imageContainer, oldImageIndex, allImages, indexCalculator) {
